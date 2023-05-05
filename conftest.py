21e968e598530111238de7a77dd4d354fe2933ff
Alexsashka43 <63096544+Alexsashka43@users.noreply.github.com>
2023-05-05 21:35:55 +0200

    ver 1.0

---
+++ conftest.py
@@ -0,0 +1,41 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as OptionsChrome
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default="en",
+                     help="Choose language")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser")
+    language = request.config.getoption("language")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        options = OptionsChrome()
+        options.add_argument("start-maximized")
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),
+                                   options=options)
+
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+@pytest.fixture(scope="function")
+def language(request):
+    return request.config.getoption("language")
